b396404e65f0feb2dba92718374a65d03c753d95
ofboox <mateuszksiazek44@gmail.com>
2026-06-20 20:14:45 +0200

    Implemented basic market actions

--- market_action.py
+++ market_action.py
@@ -3,8 +3,28 @@ from abc import ABC, abstractmethod
 class MarketAction(ABC):
 
     @abstractmethod
-    def execute(self, portfolio):
+    def execute(self):
         pass
+
+
+@dataclass
+class StockAction(MarketAction):
+    symbol: str
+    price_levels: list(PriceLevel)
+
+@dataclass
+class BuyStockAction(StockAction):
+
+    def execute(self):
+        pass
+
+
+@dataclass
+class SellStockAction(StockAction):
+
+    def execute(self):
+        pass
+
 
 
 if __name__ == '__main__':
