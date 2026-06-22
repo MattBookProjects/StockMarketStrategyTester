4f008505bd9fcd6c43054cc80830aa78b6028a28
ofboox <mateuszksiazek44@gmail.com>
2026-06-22 20:56:09 +0200

    Implemented simulation execution model

--- simulation_execution_model.py
+++ simulation_execution_model.py
@@ -4,19 +4,23 @@ from execution_model import ExecutionModel
 
 class SimulationExecutionModel(ExecutionModel):
 
+    def __init__(self, simulation_engine: SimulationEngine):
+        self.engine = simulation_engine
 
     @abstractmethod
     def send_order(self, order: Order):
-        pass
+        self.engine.send_order(order)
 
     @abstractmethod
     def cancel_order(self, order_id: str):
-        pass
+        self.enging.cancel_order(self, order_id)
 
     @abstractmethod
     def schedule_excercising_option(self, contract):
+        self.engine.schedule_excercising_option(self, contract)
         pass
     
     @abstractmethod
     def cancel_excercising_option(self, contract_id):
+        self.engine.cancel_excercising_option(self, contract_id)
         pass
